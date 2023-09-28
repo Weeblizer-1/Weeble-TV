958f401d926fd0b32789479b4eb2ee9bce77d191
Weeblizer-1 <131916107+Weeblizer-1@users.noreply.github.com>
2023-09-27 22:12:15 -0400

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,5 +1,5 @@
 window.__CONFIG__ = {
   // url must NOT end with a slash
-  VITE_CORS_PROXY_URL: "",
-  VITE_TMDB_READ_API_KEY: ""
+  VITE_CORS_PROXY_URL: "https://yes.eliasdavidmiles.workers.dev",
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhNzY5OTM2OWFlZTlhNWEyYzljYjM1Yzk1ZDZjNmViYyIsInN1YiI6IjY1MTRkY2NmOTNiZDY5MDBlMTJjMmQ0YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.-Krd4b5eW7liKktx8Tri59T_GcGHlQ9kOtNmxE4YAlk"
 };
